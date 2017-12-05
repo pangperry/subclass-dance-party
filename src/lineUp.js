f82281f7c3c473cdba51ae65773071514b398bc0
Hack Reactor Student <student@hackreactor.com>
2017-12-05 13:57:53 -0800

    starting circle lineup

--- src/lineUp.js
+++ src/lineUp.js
@@ -21,3 +21,27 @@ var lineUp = function() {
   }
 
 };
+
+
+var lineUpCircle = function() {
+
+  var centerX = $('body').height() / 2;
+  var centerY = $('body').width() / 2;
+
+  var radius = Math.min(centerX, centerY) - 10;
+
+  // var columns = 5;
+  // var rows = Math.ceil(dancers.length) / columns;
+
+  // var rowSpace = ($('body').width() - 10) / columns;
+  // var colSpace = ($('body').height() - 10) / rows;
+
+  
+  for (var i = 0; i < dancers.length; i++) {    
+    var xPos = centerX + radius * Math.sin(2 * Math.Pi * i / dancers.length);
+    var yPos = centerY + radius * Math.cos(2 * Math.Pi * i / dancers.length);
+
+    dancers[i].setPosition(yPos, xPos);
+
+  }
+};
