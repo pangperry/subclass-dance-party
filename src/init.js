fd0aba2834bfb96ae9bc2d7eef39d5617677ce08
Hack Reactor Student <student@hackreactor.com>
2017-12-04 17:17:23 -0800

    blinky dancer implemented in es6 class style

--- src/init.js
+++ src/init.js
@@ -16,18 +16,22 @@ $(document).ready(function() {
      * to the stage.
      */
     var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
-
+  
     // get the maker function for the kind of dancer we're supposed to make
     var dancerMakerFunction = window[dancerMakerFunctionName];
 
     // make a dancer with a random position
 
-    var dancer = dancerMakerFunction(
+//    var dancer = new dancerMakerFunction(
+    var dancer = new dancerMakerFunction(
+
       $("body").height() * Math.random(),
       $("body").width() * Math.random(),
-      Math.random() * 1000
+      //Math.random() * 1000
+      500
     );
     $('body').append(dancer.$node);
+    window.dancers.push(dancer);
   });
 });
 
